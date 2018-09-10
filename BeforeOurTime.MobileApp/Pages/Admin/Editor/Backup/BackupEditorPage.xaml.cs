3b8c0273d022ccb74cc447b71138b3b330f4d286
Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
2018-09-09 20:55:07 -0700

    Backup admin page no longer ignored by gitignore

---
+++ BeforeOurTime.MobileApp/Pages/Admin/Editor/Backup/BackupEditorPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BeforeOurTime.MobileApp.Pages.Admin.Editor.Backup
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BackupEditorPage : ContentPage
+    {
+        /// <summary>
+        /// Dependency injection container
+        /// </summary>
+        private IContainer Container { set; get; }
+        /// <summary>
+        /// View model
+        /// </summary>
+        private BackupEditorPageViewModel ViewModel { set; get; }
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="container">Dependency injection container</param>
+        public BackupEditorPage(IContainer container)
+        {
+            InitializeComponent();
+            Container = container;
+            BindingContext = ViewModel = new BackupEditorPageViewModel(container);
+        }
+        /// <summary>
+        /// Automatic retrieval of all items would be expensive. Use a button instead
+        /// </summary>
+        protected override void OnAppearing()
+        {
+        }
+        /// <summary>
+        /// Retrieve JSON backup of all items from server
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void ButtonLoad_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                await ViewModel.LoadJsonItems();
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Alert", "Unable to load item json backup from server. " + ex.Message, "Curses!");
+            }
+        }
+        /// <summary>
+        /// Copy json items string into system clipboard
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void ButtonCopy_Clicked(object sender, EventArgs e)
+        {
+            ViewModel.CopyToClipboard();
+            await DisplayAlert("Success", "Copied to clipboard", "Carry On");
+        }
+    }
+}
---
+++ BeforeOurTime.MobileApp/Pages/Admin/Editor/Backup/BackupEditorPageViewModel.cs
@@ -0,0 +1,62 @@
+﻿using Autofac;
+using BeforeOurTime.MobileApp.Services.Items;
+using BeforeOurTime.MobileApp.Services.Loggers;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeforeOurTime.MobileApp.Pages.Admin.Editor.Backup
+{
+    public class BackupEditorPageViewModel : BotPageVM, System.ComponentModel.INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Record errors and information during program execution
+        /// </summary>
+        private ILoggerService LoggerService { set; get; }
+        /// <summary>
+        /// Item service for CRUD operations
+        /// </summary>
+        private IItemService ItemService { set; get; }
+        public string JsonItems
+        {
+            get { return _jsonItems; }
+            set { _jsonItems = value; NotifyPropertyChanged("JsonItems"); }
+        } 
+        private string _jsonItems { set; get; }
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="container">Dependency injection controller</param>
+        public BackupEditorPageViewModel(IContainer container) : base(container)
+        {
+            LoggerService = container.Resolve<ILoggerService>();
+            ItemService = container.Resolve<IItemService>();
+        }
+        /// <summary>
+        /// Retrieve JSON backup of all items from server
+        /// </summary>
+        /// <returns></returns>
+        public async Task LoadJsonItems()
+        {
+            Working = true;
+            try
+            {
+                var items = await ItemService.ReadAsync();
+                JsonItems = JsonConvert.SerializeObject(items, Formatting.Indented);
+            }
+            finally
+            {
+                Working = false;
+            }
+        }
+        /// <summary>
+        /// Copy JSON encoded item backup to system clipboard
+        /// </summary>
+        public void CopyToClipboard()
+        {
+            Plugin.Clipboard.CrossClipboard.Current.SetText(JsonItems);
+        }
+    }
+}
