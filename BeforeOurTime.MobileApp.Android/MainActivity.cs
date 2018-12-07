6481e243fe8dc9ae30d1548fa66b507160eb1da2
Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
2018-12-06 17:35:34 -0800

    images

--- BeforeOurTime.MobileApp.Android/MainActivity.cs
+++ BeforeOurTime.MobileApp.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace BeforeOurTime.MobileApp.Droid
 {
-    [Activity(Label = "Before Our Time", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Before Our Time", Icon = "@mipmap/icon", Theme = "@style/MyTheme.Splash", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
