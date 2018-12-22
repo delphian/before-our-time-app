7ef7a7d2440367dde7052b6ed01408bb277f1e35
Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
2018-12-21 19:11:49 -0800

    Adding dpad game controller input for basic movement

--- BeforeOurTime.MobileApp.UWP/Pages/Explore/ExplorePage.xaml.cs
+++ BeforeOurTime.MobileApp.UWP/Pages/Explore/ExplorePage.xaml.cs
@@ -19,25 +19,87 @@ namespace BeforeOurTime.MobileApp.UWP.Pages.Explore
         /// </summary>
         public CustomExplorePageRenderer() : base()
         {
+//            var _timer = new DispatcherTimer { Interval = TimeSpan.FromMilliseconds(100) };
+//            _timer.Tick += ExamineControllerState;
             // When ExplorePage appears then attach to the ui core keydown event
             Loaded += (object sender, RoutedEventArgs e) =>
             {
                 Windows.UI.Core.CoreWindow.GetForCurrentThread().KeyDown += HandleKeyDown;
+//                _timer.Start();
             };
             // When ExplorePage disappears then detach from the ui core keydown event
             Unloaded += (object sender, RoutedEventArgs e) =>
             {
                 Windows.UI.Core.CoreWindow.GetForCurrentThread().KeyDown -= HandleKeyDown;
+//                _timer.Stop();
             };
+        }
+        /// <summary>
+        /// Forward a controller movement to the keypress handler
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void ExamineControllerState<TEventArgs>(object sender, TEventArgs e)
+        {
+            var gamepad = Windows.Gaming.Input.Gamepad.Gamepads.FirstOrDefault();
+            if (gamepad != null)
+            {
+                var currentState = gamepad.GetCurrentReading();
+                if (currentState.LeftThumbstickY >= 0.5)
+                {
+                    await HandleKeyInput("n");
+                }
+                if (currentState.LeftThumbstickY <= -0.5)
+                {
+                    await HandleKeyInput("s");
+                }
+                if (currentState.LeftThumbstickX >= 0.5)
+                {
+                    await HandleKeyInput("e");
+                }
+                if (currentState.LeftThumbstickX <= -0.5)
+                {
+                    await HandleKeyInput("w");
+                }
+            }
+        }
+        /// <summary>
+        /// Forward keyboard key press to the key input handler
+        /// </summary>
+        /// <param name="window"></param>
+        /// <param name="e"></param>
+        /// <returns></returns>
+        public async void HandleKeyDown(Windows.UI.Core.CoreWindow window, Windows.UI.Core.KeyEventArgs e)
+        {
+            var key = "";
+            switch (e.VirtualKey)
+            {
+                case Windows.System.VirtualKey.N:
+                case Windows.System.VirtualKey.GamepadDPadUp:
+                    key = "N";
+                    break;
+                case Windows.System.VirtualKey.S:
+                case Windows.System.VirtualKey.GamepadDPadDown:
+                    key = "S";
+                    break;
+                case Windows.System.VirtualKey.E:
+                case Windows.System.VirtualKey.GamepadDPadRight:
+                    key = "E";
+                    break;
+                case Windows.System.VirtualKey.W:
+                case Windows.System.VirtualKey.GamepadDPadLeft:
+                    key = "W";
+                    break;
+            }
+            await HandleKeyInput(key);
         }
         /// <summary>
         /// Forward a keypress to an ExplorePage method
         /// </summary>
-        /// <param name="window"></param>
-        /// <param name="e"></param>
-        public void HandleKeyDown(Windows.UI.Core.CoreWindow window, Windows.UI.Core.KeyEventArgs e)
+        /// <param name="key"></param>
+        public async Task HandleKeyInput(string key)
         {
-            (Element as ExplorePage).KeyPressed(Element, new KeyEventArgs { Key = e.VirtualKey.ToString() });
+            await (Element as ExplorePage).KeyPressed(Element, new KeyEventArgs { Key = key });
         }
     }
 }
